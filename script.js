ab024aec5c46878b37bec35e3dd4f3df064b604e
govindgb <90210096+govindgb@users.noreply.github.com>
2023-02-20 02:26:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 //your code here
-var arr = document.getElementById('ele').value;
-var obj = document.getElementById('result').textContend;
-obj = "hi";
+var arr;
+var sum = 0;
+function addElement() {
+	var input = document.getElementById('ele').value;
+    var text = String(input); 
+	arr = text.split(",");
+	//alert(input);
+    //console.log(arr[0]);
+   
+	 calculateMinimumCost();
+}
+function calculateMinimumCost() {
+    
+	while (1) {
+        //console.log(arr,sum);
+		if(arr.length == 1) break;
+        arr.sort(function(a,b) {return a-b}) ;
+		var ele = Number(arr[0])+Number(arr[1]);
+		arr.splice(0,2);
+        arr.unshift(ele);
+		sum += Number(ele);
+        //document.write(arr,sum);
+	}
+	var val=document.getElementById('result');
+	val.textContent = sum;
+}
