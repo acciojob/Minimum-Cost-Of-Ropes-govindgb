1ef320e865b8b244a23f0cca58c5c753a1d55f87
govindgb <90210096+govindgb@users.noreply.github.com>
2023-02-20 11:53:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ function addElement(event) {
 }
 function calculateMinimumCost() {
     
-	while (1) {
+	while (arr.length > 1) {
         //console.log(arr,sum);
-		if(arr.length == 1) break;
+		//if(arr.length == 1) break;
         arr.sort(function(a,b) {return a-b}) ;
 		var ele = Number(arr[0])+Number(arr[1]);
 		arr.splice(0,2);
