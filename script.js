472b4c2f36e2d511d8144c86f4a9a0e6444adeca
govindgb <90210096+govindgb@users.noreply.github.com>
2023-02-21 23:29:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-//your code here
-var arr;
-var sum = 0;
-function addElement(event) {
-	event.preventDefault();
-	var input = document.querySelector('input').value;
-    var text = String(input); 
-	arr = text.split(",");
-	//alert(input);
-    //console.log(arr[0]);
-   
-	 calculateMinimumCost();
-}
-function calculateMinimumCost() {
-    
-	while (arr.length > 1) {
-        //console.log(arr,sum);
-		//if(arr.length == 1) break;
-        arr.sort(function(a,b) {return a-b}) ;
-		var ele = Number(arr[0])+Number(arr[1]);
-		arr.splice(0,2);
-        arr.unshift(ele);
-		sum += Number(ele);
-        //document.write(arr,sum);
-	}
-	var val=document.getElementById('result');
-	val.textContent = sum;
+function minCostToFormRope(a) {
+  a.preventDefault();
+  var inputElement = document.querySelector("input").value;
+  var arr = inputElement.split(",");
+  arr.sort(function (a, b) {
+    return a - b;
+  });
+
+  // we need to access first two element
+  // add them & store in a variable res
+
+  // add the res in array
+  // increment cost by res
+  var cost = 0;
+  while (arr.length > 1) {
+    var res = Number(arr[0]) + Number(arr[1]);
+    arr.splice(0, 2);
+
+    arr.push(res);
+    cost += res;
+
+    arr.sort(function (a, b) {
+      return a - b;
+    });
+  }
+
+  document.getElementById("result").textContent = cost;
 }
